d530833691719c21c02ec56b88ad1e5883b160d4
Hiure <hiure@riseup.net>
2018-03-08 11:24:58 -0300

    Nova versão do voucher admin em um unico script
    Nessa versão o script verifica se há alterações antes de escrever no flash do roteador, ele ainda vem com uns loops inteligentes para não ficar saindo do script automaticamente.

---
+++ voucherAdmin-v2.0.sh
@@ -0,0 +1,269 @@
+clear
+op=1
+mv db.csv db2.csv
+scp root@thisnode.info:/etc/nodogsplash/vale/db.csv ./
+while [ ! $op -eq 6 ]; do
+  clear
+  echo "--------------------------------------------------------------------------------"
+  echo "|----------------------------Digite a opção desejada:--------------------------|"
+  echo "|                                                                              |"
+  echo "|1- Listar todos os vouchers cadastrados.                                      |"
+  echo "|2- Cadastrar voucher para novo usuario.                                       |"
+  echo "|3- Consultar prazo de validade do voucher.                                    |"
+  echo "|4- Remover usuario/voucher.                                                   |"
+  echo "|5- Editar usuario/voucher.                                                    |"
+  echo "|6- Sair.                                                                      |"
+  echo "|                                                                              |"
+  echo "--------------------------------------------------------------------------------"
+  read op
+  case $op in
+
+    1) clear
+
+    echo Eu tenho `cat db.csv | wc -l` vouchers cadastrados
+    echo "Que São:"
+    sort -o db.csv db.csv
+    cat db.csv
+
+      echo "Aperte uma tecla para continuar..."
+      read
+      ;;
+
+    2) clear
+    cont=1
+      while [ ! $cont -eq 2 ]; do
+      echo " ___________________________________________________ "
+      echo "|                                                   |"
+      echo "|                                                   |"
+      echo "|    Para cadastrar um novo usuario digite          |"
+      echo "|    um nome e uma sequencia com 12 digitos         |"
+      echo "|                                                   |"
+      echo "|___________________________________________________|"
+
+
+      printf "Digite o nome do usuario: "
+      read nome
+
+        if [ ${#nome}=0 ]
+        then
+        printf "Digite um voucher de 12 digitos: "
+        read voucher
+	        if [ ${#voucher} -ne 12 ]
+	        then
+	        echo "O voucher que vc digitou tem ${#voucher}, e ele precisa ter 12 digitos"
+	        else
+	        echo "$nome,$voucher,," >> db.csv
+          sort -o db.csv db.csv
+	        echo "Usuario $nome cadastrado com o voucher $voucher"
+          fi
+        else
+        echo "Não deixe o nome em branco, isso atrapalha a vida!"
+        fi
+      cat db.csv
+      printf "Deseja cadastrar usuario novamente?(s ou n)"
+      read cont
+        case $cont in
+          S|s|sim|SIM|Y|yes|YES) cont=1
+          ;;
+          N|n|nao|Nao|NO|no) cont=2
+         ;;
+        *) echo "Opcao invalida"
+          echo "Aperte uma tecla para continuar..."
+          read
+        esac #fim do while 8
+      done
+    ;;
+    3) clear
+      consulta=1
+      while [ ! $consulta -eq 2 ]; do
+      hoje="$(date +%s)"
+      vale_secs=2592000 #seconds = 30 days
+      echo " ___________________________________________________ "
+      echo "|                                                   |"
+      echo "|                                                   |"
+      echo "| Este opção vai te ajudar a consultar o prazo de   |"
+      echo "| validade do voucher de user cadastrado.           |"
+      echo "|___________________________________________________|"
+
+      printf "Para consultar digite o nome do usuario cadastrado "
+      read nome
+
+        if [ ${#nome}=0 ]
+        then
+        cat db.csv |grep "$nome" | cut -f3 -d "," > usertemp
+        cat db.csv |grep "$nome" | cut -f1 -d "," > user
+          cat usertemp | while read temp
+           do
+           rest=$[(($temp-$hoje)+$vale_secs)/86400]
+            if [ $rest -lt 0  ]
+            then
+            echo "O voucher associado a esse usuario ainda não foi utilizado"
+            else
+            echo faltam $rest dias para acabar o voucher do user $nome
+            fi
+           done
+        else
+        echo "Não deixe o nome em branco, isso atrapalha a vida!"
+        fi
+      printf "Deseja consultar prazo para usuario novamente?(s ou n)"
+      read consulta
+      case $consulta in
+        S|s|sim|SIM|Y|yes|YES) consulta=1
+          ;;
+        N|n|nao|Nao|NO|no) consulta=2
+         ;;
+        *) echo "Opcao invalida"
+          echo "Aperte uma tecla para continuar..."
+          read
+      esac #fim do while 8
+    done
+
+   echo "Aperte uma tecla para continuar..."
+   read
+   ;;
+
+    4) clear # ???
+    remval=1
+    while [ ! $remval -eq 2 ]; do
+    echo " ___________________________________________________ "
+    echo "|                                                   |"
+    echo "|                                                   |"
+    echo "| Menu para REMOVER usuarios, para isso digite      |"
+    echo "| exatamente o nome como no cadastro                |"
+    echo "|                                                   |"
+    echo "|___________________________________________________|"
+
+    echo "-----------------------------------------------------"
+    echo "Os usuarios cadastrados são:"
+    cat user
+
+    printf "Nome do user a remover: "
+    read nome
+
+    if [ ${#nome}=0 ]
+    then
+
+    grep -v "$nome" db.csv > /tmp/$$
+    mv /tmp/$$ db.csv
+    cat db.csv | cut -f1 -d "," > user
+
+    echo "Nome removido com sucesso, até mais!"
+
+    else
+    echo "Não deixe o nome em branco, isso atrapalha a vida!"
+    fi
+    printf "Deseja remover mais usuarios?(s ou n)"
+      read remval
+      case $remval in
+        S|s|sim|SIM|Y|yes|YES) remval=1
+          ;;
+        N|n|nao|Nao|NO|no) remval=2
+         ;;
+        *) echo "Opcao invalida"
+          echo "Aperte uma tecla para continuar..."
+          read
+      esac #fim do while 8
+    done
+
+     echo "Aperte uma tecla para continuar..."
+     read
+     ;;
+
+    5) clear # ???
+    remval=1
+    while [ ! $remval -eq 2 ]; do
+
+    echo " ___________________________________________________ "
+    echo "|                                                   |"
+    echo "|                                                   |"
+    echo "| Programa para editar usuarios, para isso digite   |"
+    echo "| o nome como no cadastro, e depois um voucher      |"
+    echo "|                                                   |"
+    echo "|___________________________________________________|"
+
+    printf "Nome do user para editar: "
+    read nome
+
+      if [ ${#nome}=0 ]
+      then
+      grep -v "$nome" db.csv > /tmp/$$
+      mv /tmp/$$ db.csv
+
+      printf "Digite um voucher de 12 digitos: "
+      read voucher
+        if [ ${#voucher} -ne 12 ]
+        then
+        echo "O voucher que vc digitou tem ${#voucher}, e ele precisa ter 12 digitos"
+        else
+        echo "$nome,$voucher,," >> db.csv
+        sort -o db.csv db.csv
+        fi
+      else
+      echo "Não deixe o nome em branco, isso atrapalha a vida!"
+      fi
+    printf "Deseja editar mais usuarios?(s ou n)"
+      read remval
+      case $remval in
+        S|s|sim|SIM|Y|yes|YES) remval=1
+          ;;
+        N|n|nao|Nao|NO|no) remval=2
+         ;;
+        *) echo "Opcao invalida"
+          echo "Aperte uma tecla para continuar..."
+          read
+      esac #fim do while 8
+    done
+
+    echo "Aperte uma tecla para continuar..."
+    read
+    ;;
+
+    6) clear
+      echo "Deseja realmente sair?"
+      read res
+      case $res in
+        S|s|sim|SIM|Y|yes|YES) op=6
+
+          if diff db.csv db2.csv >/dev/null ; then
+            echo "Nenhuma alteração foi encontrada"
+          else
+
+              printf "Deseja salvar as alterações no Gateway?: "
+              read respb
+              case $respb in
+              S|s|sim|SIM|Y|yes|YES)
+              scp ./db.csv root@thisnode.info:/etc/nodogsplash/vale/db.csv
+              echo "Alterações realiadas com sucesso!"
+              ;;
+              N|n|nao|Nao|NO|no)
+                echo "Ok, as alterações feitas na secção não foram implementadas Gataway"
+                ;;
+              *) echo "Opção invalida"
+                echo "aperte uma tecla para continuar"
+              esac
+          fi
+          cat db.csv
+              ;;
+        N|n|nao|Nao|NO|no) op=0
+          ;;
+        *) echo "Opcao invalida"
+          echo "Aperte uma tecla para continuar..."
+          read
+      esac #fim do while 8
+      ;;
+
+    7|8|9|1[0-2]) clear
+      echo "Opcao ainda nao implementada."
+      echo "Aperte uma tecla para continuar..."
+      read
+      ;;
+
+    *) clear
+      echo "Opcao invalida."
+      echo "Aperte uma tecla para continuar..."
+      read
+      ;;
+
+    esac #fim do case
+
+done #fim do while
